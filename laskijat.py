b4a04cbd90d1d61e89c906deb525a12585612e92
MariaLindblom <82643363+MariaLindblom@users.noreply.github.com>
2022-02-17 09:23:20 +0200

    Update laskijat.py

--- laskijat.py
+++ laskijat.py
@@ -35,6 +35,39 @@ class Laskija:
 
 
 ### Lisää MonenLaskija ja argumenttien_tulostaja tähän.
+class Monenlaskija(Laskija):
+    """Luokka, joka toteuttaa eri laskutoimituksia.
+
+    Julkiset metodit:
+        summaa(Union[int, float], Union[int, float])
+        kerro(Union[int, float], Union[int, float])
+    """
+    def summaa(self, a, b):
+        """Palauttaa kahden luvun summan.
+
+        :param a: summan ensimmäinen luku
+        :type a: Union[int, float]
+        :param b: summan toinen luku
+        :type b: Union[int, float]
+        :return: lukujen a ja b summa
+        :rtype: Union[int, float]
+        """
+        return sum([a, b])
+
+    def kerro(self, a, b):
+        """Palauttaa kahden luvun tulon.
+
+        :param a: tulon ensimmäinen luku
+        :type a: Union[int, float]
+        :param b: tulon toinen luku
+        :type b: Union[int, float]
+        :return: lukujen a ja b tulo
+        :rtype: Union[int, float]
+        """
+        tulo = 1
+        for luku in [a, b]:
+            tulo *= luku
+        return tulo
 
 
 
