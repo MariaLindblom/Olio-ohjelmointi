1b8a3236dd522fb4c32f2513c4dd6792caf68ee9
MariaLindblom <82643363+MariaLindblom@users.noreply.github.com>
2022-03-02 13:49:32 +0200

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -1,5 +1,24 @@
+import random
+
 class Palvelu:
+  """Luokka, joka kuvaa palvelua."""  
+  
+  def __init__(self,tuotenimi, asiakkaat):
+    """Konstruktori"""
+    
 
 class ParempiPalvelu(Palvelu):
+  """Luokka, joka kuvaa parempaa palvelua."""
+  
+  def __init__(self, edut):
+    """Konstruktori."""
+    #_self.edut = ("...")
   
 class Asiakas:
+  """Luokka, joka kuvaa asikasta."""
+  
+  def __init__(self, nimi, asiakasnro, ika):
+    """Konstruktori."""
+    _self.nimi = ("...")
+    _self.asiakasnro = ("...")
+    _self.ika = ("...")
