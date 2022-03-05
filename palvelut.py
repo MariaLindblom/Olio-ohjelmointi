3ea09973e9be13243b90c42605808d60f402792e
MariaLindblom <82643363+MariaLindblom@users.noreply.github.com>
2022-03-05 19:20:51 +0200

    Update palvelut.py

--- palvelut.py
+++ palvelut.py
@@ -1,7 +1,20 @@
 import random
 
 class Palvelu:
-  """Luokka, joka kuvaa palvelua."""  
+  """Luokka, joka kuvaa palvelua.
+        :ivar tuotenimi: tuotteen nimi
+        :type tuotenimi: str
+        :ivar asiakkaat: lista asiakkaista 
+        :type asiakkaat: list
+
+        Julkiset metodit
+            lisaa_asiakas(asiakas)
+            poista_asiakas(asiakas)
+            tulosta_asiakkaat(asiakas)
+
+        Salatut metodit
+            luo_asiakasrivi(asiakas)
+        """  
   
   def __init__(self,tuotenimi, asiakkaat):
     """Konstruktori"""
@@ -21,11 +34,19 @@ class Palvelu:
     
 
 class ParempiPalvelu(Palvelu):
-  """Luokka, joka kuvaa parempaa palvelua."""
+  """Luokka, joka kuvaa parempaa palvelua.
+        :ivar edut: paremman palvelun edut
+        :type edut: str
+
+    Julkiset palvelut
+    lisaa_etu
+    poista_etu
+    tulosta_edut
+"""
   
   def __init__(self, edut):
     """Konstruktori."""
-    #_self.edut = ("...")
+    _self.edut = ("...")
     
   def lisaa_etu():
     
@@ -37,7 +58,17 @@ class ParempiPalvelu(Palvelu):
   
   
 class Asiakas:
-  """Luokka, joka kuvaa asiakasta."""
+  """Luokka, joka kuvaa asiakasta.
+        :ivar nimi: asiakkaan nimi
+        :type nimi: str
+        :ivar asiakasnro: asiakkaan numero
+        :type asiakasnro: int[]
+        :ivar ika: asiakkaan ikä
+        :type ika: int
+
+        Yksityiset metodit
+        luo_nro()
+    """
   
   def __init__(self, nimi, asiakasnro, ika):
     """Konstruktori."""
